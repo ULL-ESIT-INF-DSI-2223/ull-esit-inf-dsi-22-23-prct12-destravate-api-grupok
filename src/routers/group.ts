eec82a31caf11f980648884ddacfda70aa84a882
Aday Chocho <alu0101437538@ull.edu.es>
2023-05-10 17:09:56 +0000

    -z

--- src/routers/group.ts
+++ src/routers/group.ts
@@ -7,7 +7,7 @@ groupRouter.use(express.json());
 
 groupRouter.post('/groups', async (req, res) => {
   const group = new Group(req.body);
-  
+
   try {
     await group.save()
     res.status(201).send(group);
@@ -15,3 +15,32 @@ groupRouter.post('/groups', async (req, res) => {
     res.status(400).send(err);
   }
 });
+
+/**
+ * Get para todos los grupos especificos
+ */
+groupRouter.get('/groups', async (req, res) => {
+    const filter = req.query.name?{name: req.query.name.toString()}:{};
+  
+    try {
+      const groups = await Group.find(filter);
+  
+      if (groups.length !== 0) {
+        return res.send(groups);
+      }
+      return res.status(404).send();
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  });
+  
+  /**
+   * Get para un group en específico
+   */
+  groupRouter.get('/groups/:id', async (req, res) => {
+    const group = await Group.findOne({ ID: req.params.id });
+    if (!group) {
+      res.status(404).send();
+    }
+    res.send(group);
+  });
