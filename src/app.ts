2e35a761eda28cd640004534caafec54841381b4
Aday Chocho <alu0101437538@ull.edu.es>
2023-05-10 16:37:21 +0000

    -z

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import './db/mongoose.js';
 import { userRouter } from './routers/user.js';
+import { trackRouter } from './routers/track.js';
+import { groupRouter } from './routers/group.js';
+import { challengeRouter } from './routers/challenge.js';
 
 export const app = express();
 app.use(express.json());
 app.use(userRouter);
+app.use(groupRouter);
+app.use(trackRouter);
+app.use(challengeRouter);
