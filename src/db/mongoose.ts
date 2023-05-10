7b5f47be5495345c25161138ace0da6720963fa2
Aday Chocho <alu0101437538@ull.edu.es>
2023-05-10 16:46:32 +0000

    -z

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,7 +1,8 @@
 import { connect } from 'mongoose';
 
-connect('mongodb://127.0.0.1:27017/tracks-api').then(() => {
+try {
+  await connect(process.env.MONGODB_URL!);
   console.log('Connection to MongoDB server established');
-}).catch(() => {
-  console.log('Unable to connect to MongoDB server');
-});
+} catch(error) {
+  console.log(error);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from './app.js';
 
-const port = 3000//process.env.PORT;
+const port = 3000 || process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
