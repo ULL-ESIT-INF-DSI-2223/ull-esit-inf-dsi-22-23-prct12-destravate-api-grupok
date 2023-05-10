324c2e651188b2e40a58c94b0d4f091888698546
SergioPerera <alu0101394503@ull.edu.es>
2023-05-10 16:58:34 +0000

    BackUp

--- src/routers/track.ts
+++ src/routers/track.ts
@@ -15,3 +15,18 @@ trackRouter.post('/tracks', async (req, res) => {
     res.status(400).send(err);
   }
 });
+
+trackRouter.get('/tracks', async (req, res) => {
+  const tracks = await Track.find({});
+  res.send(tracks);
+});
+
+trackRouter.get('/tracks/:id', async (req, res) => {
+  const track = await Track.findById(req.params.id);
+  if (!track) {
+    res.status(404).send();
+  }
+  res.send(track);
+});
+
+
