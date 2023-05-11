913b499f0ccda117469d2897c83dd8617a558890
Brayan Jose Perez Ramos <alu0101431185@ull.edu.es>
2023-05-11 22:59:45 +0000

    patch sync

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { User } from "../models/user.js";
 import { Group } from "../models/group.js";
 import { Challenge } from "../models/challenge.js";
+import { Track } from "../models/track.js";
 
 export const userRouter = express.Router();
 
@@ -144,7 +145,11 @@ userRouter.patch("/users", async (req, res) => {
         case "tracksHistory":
           // borrar de los tracks en los que es participante
           await Track.updateMany({ users: user._id },{ $pull: { users: user._id }});
-          for(const trackID of req.body.tracksHistory.) {
+          for(const track of req.body.tracksHistory) {
+            const trackID = track.track;
+            await Track.findByIdAndUpdate(trackID, { $push: { users: user._id }});
+          }
+        break;
         default:
         break;
       }
@@ -185,6 +190,35 @@ userRouter.patch("/users/:id", async (req, res) => {
     );
     if (!user) {
       return res.status(404).send();
+    }
+    // recorrer updates y actualizar las demas cosas en cada caso
+    for (const update of updates) {
+      switch (update) {
+        case "groups":
+          // borrar de los grupos en los que es participante
+          await Group.updateMany({ members: user._id },{ $pull: { members: user._id }});
+          for(const groupID of req.body.groups) {
+            await Group.findByIdAndUpdate(groupID, { $push: { members: user._id }});
+          }
+        break;
+        case "activeChallenges":
+          // borrar de los challenge en los que es participante
+          await Challenge.updateMany({ users: user._id },{ $pull: { users: user._id }});
+          for(const challengeID of req.body.activeChallenges) {
+            await Challenge.findByIdAndUpdate(challengeID, { $push: { users: user._id }});
+          }
+        break;
+        case "tracksHistory":
+          // borrar de los tracks en los que es participante
+          await Track.updateMany({ users: user._id },{ $pull: { users: user._id }});
+          for(const track of req.body.tracksHistory) {
+            const trackID = track.track;
+            await Track.findByIdAndUpdate(trackID, { $push: { users: user._id }});
+          }
+        break;
+        default:
+        break;
+      }
     }
     return res.status(200).send(user);
   } catch (err) {
